1ad542a54266b547533765ebc8beabb8632a2e7c
mehedihasan101101 <sojibkhan39621@gmail.com>
2025-07-26 14:56:54 +0100

    carousel added

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -1,18 +1,45 @@
+import { Link, useLoaderData } from "react-router";
 import BreakingNewsHeadline from "../components/BreakingNewsHeadline";
-import NavBar from "../components/NavBar";
-
-
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+const responsive = {
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 8
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 6
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+    }
+}
 const Home = () => {
+    const categories = useLoaderData();
+    console.log(categories)
     return (
         <>
             {/* Breaking News Headlines */}
-            <BreakingNewsHeadline>
+            <BreakingNewsHeadline></BreakingNewsHeadline>
 
-            </BreakingNewsHeadline>
+            <section className="mt-2 lg:hidden">
+                <Carousel className="flex" responsive={responsive} infinite={true} removeArrowOnDeviceType={["tablet", "mobile", "desktop"]}>
+                    {
+                        categories.map(eachCategory => <div><p>{eachCategory.category}</p></div>)
+                    }
+                </Carousel>
+
+            </section>
+
             <div className="grid grid-cols-12">
+                {/* News section */}
                 <div className="col-span-10">
                     <h1>News</h1>
                 </div>
+
+                {/* Aside for other contents */}
                 <aside className="col-span-2">
                     <h1>Aside</h1>
                 </aside>
