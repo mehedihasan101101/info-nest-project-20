de544be9ecf7ce5ae7cc073274caca16fcc69a56
mehedihasan101101 <sojibkhan39621@gmail.com>
2025-08-17 02:12:11 +0100

    Set minimum height for dashboard container

--- src/components/BookmarkCard.jsx
+++ src/components/BookmarkCard.jsx
@@ -18,7 +18,7 @@ const BookmarkCard = ({ eachMarkedNews }) => {
     return (
         <div className="flex items-center justify-between w-full gap-3 bg-gray-200/40 py-3 px-3 rounded">
             <div className="flex gap-3">
-                <img className="w-35 h-20 rounded" src={image_url} alt="" />
+                <img className="w-35 md:h-20 rounded" src={image_url} alt="" />
                 <div>
                     <h5 className="font-bold">{title}</h5>
                     <p className="">Author: {author.name}</p>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,29 +2,43 @@ import { useContext } from "react";
 import { AutContext } from "../Context/AuthContext";
 import userAvatar from "./../assets/user.png"
 import BookmarkCard from "./../components/BookmarkCard"
-
+import LoadingPage from "./../pages/LoadingPage"
+import { useNavigate } from "react-router";
 const Dashboard = () => {
-    const { user, bookmarkedNews } = useContext(AutContext)
+    const { user, bookmarkedNews, loading } = useContext(AutContext);
+    const navigate = useNavigate()
 
-    return (
-        <div className="grid gap-3 grid-cols-12">
-            <section className="col-span-3 rounded space-y-2  flex flex-col items-center p-3 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
+    if (loading) {
+        return <LoadingPage></LoadingPage>
+    }
+    if (user) {
+        return (
 
-                <img className="w-60 rounded-full" src={user?.photoURL ? user.photoURL : userAvatar} alt="" />
-                <h5 className="font-bold">{user?.displayName}</h5>
-                <p className="font-bold">Email:<span className="ml-1 font-medium">{user?.email}</span></p>
-                <p className="font-bold">Account Status:<span className="text-green-500 font-medium ml-2">{user?.emailVerified && "Verified"}</span></p>
-            </section>
+                <div className="md:grid flex flex-col gap-3 grid-cols-12 lg:min-h-[55vh]">
+                    <section className="lg:col-span-3 md:col-span-4 col-span-6 rounded space-y-2  flex flex-col items-center p-3 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
 
-            <section className="col-span-9 flex items-center flex-col p-3 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-                <h1 className="font-bold text-xl">Bookmarked News</h1>
-                <div className="w-full grid grid-cols-2 gap-2">
-                    {bookmarkedNews.map(eachMarkedNews => <BookmarkCard key={eachMarkedNews._id} eachMarkedNews={eachMarkedNews}></BookmarkCard>)}
+                        <img className="md:w-60 w-35 rounded-full" src={user?.photoURL ? user.photoURL : userAvatar} alt="" />
+                        <h5 className="font-bold">{user?.displayName}</h5>
+                        <p className="font-bold">Email:<span className="ml-1 font-medium">{user?.email}</span></p>
+                        <p className="font-bold">Account Status:<span className="text-green-500 font-medium ml-2">{user?.emailVerified && "Verified"}</span></p>
+                    </section>
 
+                    <section className="lg:col-span-9 md:col-span-8  flex items-center flex-col p-3 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
+                        <h1 className="font-bold text-xl">Bookmarked News</h1>
+                        <div className="w-full grid lg:grid-cols-2 gap-2">
+                            {bookmarkedNews.map(eachMarkedNews => <BookmarkCard key={eachMarkedNews._id} eachMarkedNews={eachMarkedNews}></BookmarkCard>)}
+
+                        </div>
+                    </section>
                 </div>
-            </section>
-        </div>
-    );
+
+
+        );
+    }
+    if (!user) {
+        navigate('/')
+    }
+
 };
 
 export default Dashboard;
